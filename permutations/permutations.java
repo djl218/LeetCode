1850a9d5a14ee04ab411029129a18b5a47a8b789
Daniel Leskosky <dleskosky@gmail.com>
2021-04-16 19:12:48 -0400

    Time: 1 ms (92.48%), Space: 39 MB (71.96%) - LeetHub

---
+++ permutations/permutations.java
@@ -0,0 +1,47 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Queue<List<Integer>> permutations = new LinkedList<>();
+        permutations.add(new ArrayList<>());
+        for (int currentNumber : nums) {
+            int n = permutations.size();
+            for (int i = 0; i < n; i++) {
+                List<Integer> oldPermutation = permutations.poll();
+                for (int j = 0; j <= oldPermutation.size(); j++) {
+                    List<Integer> newPermutation = new ArrayList<Integer>(oldPermutation);
+                    newPermutation.add(j, currentNumber);
+                    if (newPermutation.size() == nums.length) {
+                        result.add(newPermutation);
+                    } else {
+                        permutations.add(newPermutation);
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
+/*
+class Solution {
+    List<List<Integer>> result = new ArrayList<>();
+    
+    public List<List<Integer>> permute(int[] nums) {
+        backtrack(nums, new ArrayList<>());
+        return result;
+    }
+    
+    private void backtrack(int[] nums, List<Integer> list) {
+        if (list.size() == nums.length) {
+            result.add(new ArrayList(list));
+            return;
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (list.contains(nums[i])) continue;
+            list.add(nums[i]);
+            backtrack(nums, new ArrayList(list));
+            list.remove(list.size() - 1);
+        }
+    }
+}
+*/
