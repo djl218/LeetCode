287fa03d14accd2a81baf104c8e6235e94d2ec46
Daniel Leskosky <dleskosky@gmail.com>
2021-04-16 22:30:23 -0400

    Time: 18 ms (20.86%), Space: 36 MB (95.72%) - LeetHub

---
+++ sudoku-solver/sudoku-solver.java
@@ -0,0 +1,42 @@
+// m = board.length, n = board[0].length
+// Time: O(9^(m*n))
+// Space: O(m*n)
+class Solution {
+    public void solveSudoku(char[][] board) {
+        if (board == null || board.length == 0)
+            return;  
+        solve(board);
+    }
+    
+    private boolean solve(char[][] board) {
+        int m = board.length, n = board[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == '.') {
+                    for (char c = '1'; c <= '9'; c++) {
+                        if (!(isValid(board, c, i, j))) {
+                            continue;
+                        } else {
+                            board[i][j] = c;
+                            if (solve(board))
+                                return true;
+                            else
+                                board[i][j] = '.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    private boolean isValid(char[][] board, char c, int row, int col) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == c) return false;
+            if (board[i][col] == c) return false;
+            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c) return false;
+        }
+        return true;
+    }
+}
