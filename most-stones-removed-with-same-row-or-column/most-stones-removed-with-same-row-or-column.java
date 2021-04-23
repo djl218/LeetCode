35e53a3fa16e159737387bbacf3655708fcb6d69
Daniel Leskosky <dleskosky@gmail.com>
2021-04-23 10:25:25 -0400

    Time: 227 ms (6.58%), Space: 39.2 MB (49.27%) - LeetHub

---
+++ most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.java
@@ -0,0 +1,25 @@
+class Solution {
+    // Ans = # of stones – # of islands
+    public int removeStones(int[][] stones) {
+        Set<int[]> visited = new HashSet();
+        int numOfIslands = 0;
+        for (int[] s1:stones){
+            if (!visited.contains(s1)){
+               dfs(s1, visited, stones); 
+               numOfIslands++;
+            }
+        }
+        return stones.length - numOfIslands;
+    }
+    
+    private void dfs(int[] s1, Set<int[]> visited, int[][] stones){
+        visited.add(s1);
+        for (int[] s2: stones){
+            if (!visited.contains(s2)){
+				// stone with same row or column. group them into island
+                if (s1[0] == s2[0] || s1[1] == s2[1])
+                    dfs(s2, visited, stones);
+            }
+        }
+    }
+}
