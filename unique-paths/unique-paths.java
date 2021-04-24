9782c93c230b36c55c4bc45ed2d8d4343269e4cd
Daniel Leskosky <dleskosky@gmail.com>
2021-04-24 13:14:36 -0400

    Update unique-paths.java

--- unique-paths/unique-paths.java
+++ unique-paths/unique-paths.java
@@ -1,19 +1,36 @@
-// Top-down dynamic programming with memoization
+// Brute force recursion
 // Time: O(2^(m + n))
-// Space: O(m + n)
+// Space: O(m + n) for recursion stack
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m+1][n+1];
-        return CountWaysRecursive(dp, m, n);
+        return dfs(m, n);
     }
     
-    public int CountWaysRecursive(int[][] dp, int m, int n) {
-        if (m == 0 || n == 0) return 1;
+    private int dfs(int m, int n) {
+        if (m == 1 || n == 1) return 1;
+        
+        int moveRight = dfs(m, n - 1);
+        int moveDown = dfs(m - 1, n);
+        
+        return moveRight + moveDown;
+    }
+}
+
+// Top-down dynamic programming with memoization
+// Time: O(m * n)
+// Space: O(m * n)
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m + 1][n + 1];
+        return dfs(dp, m, n);
+    }
+    
+    public int dfs(int[][] dp, int m, int n) {
         if (m == 1 || n == 1) return 1;
         
         if (dp[m][n] == 0) {
-            int moveRight = n > 0 ? CountWaysRecursive(dp, m, n - 1) : 0;
-            int moveDown = m > 0 ? CountWaysRecursive(dp, m - 1, n) : 0;
+            int moveRight = dfs(dp, m, n - 1);
+            int moveDown = dfs(dp, m - 1, n);
             
             dp[m][n] = moveRight + moveDown;
         }
@@ -27,12 +44,12 @@ class Solution {
 // Space: O(m * n)
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m+1][n+1];
+        int[][] dp = new int[m + 1][n + 1];
         dp[0][1] = 1;
 
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+        for (int i = 1; i < m + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
             }
         }
         
