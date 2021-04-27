3c60a8989e3fd80d327709fbdcb1672506db6edc
Daniel Leskosky <dleskosky@gmail.com>
2021-04-27 10:29:04 -0400

    Update minimum-path-sum.java

--- minimum-path-sum/minimum-path-sum.java
+++ minimum-path-sum/minimum-path-sum.java
@@ -1,4 +1,3 @@
-/*
 // Brute force recursion
 // Time: O(2^(m + n))
 // Space: O(m + n) for recursion stack
@@ -25,8 +24,10 @@ class Solution {
         dfs(grid, row, col + 1, sum);         
     }
 }
-*/
 
+// Top-down dynamic programming with memoization
+// Time: O(m * n)
+// Space: O(m * n)
 class Solution {
     int m, n;
     
@@ -55,5 +56,25 @@ class Solution {
         
         memo[row][col] = grid[row][col] + Math.min(down, right);
         return memo[row][col];
+    }
+}
+
+// Official LeetCode solution
+// Dynamic programming - no extra space
+// Time: O(m * n)
+// Space: O(1)
+class Solution {
+    public int minPathSum(int[][] grid) {
+        for (int i = grid.length - 1; i >= 0; i--) {
+            for (int j = grid[0].length - 1; j >= 0; j--) {
+                if(i == grid.length - 1 && j != grid[0].length - 1)
+                    grid[i][j] = grid[i][j] +  grid[i][j + 1];
+                else if(j == grid[0].length - 1 && i != grid.length - 1)
+                    grid[i][j] = grid[i][j] + grid[i + 1][j];
+                else if(j != grid[0].length - 1 && i != grid.length - 1)
+                    grid[i][j] = grid[i][j] + Math.min(grid[i + 1][j],grid[i][j + 1]);
+            }
+        }
+        return grid[0][0];
     }
 }
