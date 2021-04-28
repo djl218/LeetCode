77c5b173c4a3b6b98a59bdd398247c194e092302
Daniel Leskosky <dleskosky@gmail.com>
2021-04-28 12:52:23 -0400

    Update course-schedule.java

--- course-schedule/course-schedule.java
+++ course-schedule/course-schedule.java
@@ -1,35 +1,52 @@
+// Topological Sort
+// Time and space complexity is O(numCourses + prerequisites.length)
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        Map<Integer, Integer> inDeg = new HashMap<>();
-        Map<Integer, List<Integer>> adj = new HashMap<>();
+        List<Integer> sortedOrder = new ArrayList<>();
         
+        // a. Initialize the graph
+        HashMap<Integer, Integer> inDegree = new HashMap<>(); 
+        // adjacency list
+        HashMap<Integer, List<Integer>> graph = new HashMap<>();
         for (int i = 0; i < numCourses; i++) {
-            inDeg.put(i, 0);
-            adj.put(i, new ArrayList<>());
+            inDegree.put(i, 0);
+            graph.put(i, new ArrayList<Integer>());
         }
         
-        for (int[] pre : prerequisites) {
-            inDeg.put(pre[0], inDeg.get(pre[0]) + 1);
-            adj.get(pre[1]).add(pre[0]);
+        // b. Build the graph
+        for (int i = 0; i < prerequisites.length; i++) {
+            // make sure to correctly identify the parent and the child
+            int parent = prerequisites[i][1];
+            int child = prerequisites[i][0];
+            // put the child into it's parent's list
+            graph.get(parent).add(child);
+            // increment child's inDegree
+            inDegree.put(child, inDegree.get(child) + 1);
         }
         
+        // c. Find all sources i.e., all vertices with 0 in-degrees
         Queue<Integer> sources = new LinkedList<>();
-        for (int key : inDeg.keySet()) {
-            if (inDeg.get(key) == 0)
-                sources.offer(key);
-        }
-        
-        List<Integer> sorted = new ArrayList<>();
-        while (!sources.isEmpty()) {
-            int curr = sources.poll();
-            sorted.add(curr);
-            for (int nei : adj.get(curr)) {
-                inDeg.put(nei, inDeg.get(nei) - 1);
-                if (inDeg.get(nei) == 0)
-                    sources.offer(nei);
+        for (Map.Entry<Integer, Integer> entry : inDegree.entrySet()) {
+            if (entry.getValue() == 0) {
+                sources.add(entry.getKey());
             }
         }
-        System.out.print(sorted.size() + "" + numCourses);
-        return sorted.size() == numCourses;
+        
+        // d. For each source, add it to the sortedOrder and subtract one 
+        // from all of its children's in-degrees
+        // if a child's in-degree becomes zero, add it to the sources queue
+        while (!sources.isEmpty()) {
+            int vertex = sources.poll();
+            sortedOrder.add(vertex);
+            List<Integer> children = graph.get(vertex);
+            for (int child : children) {
+                inDegree.put(child, inDegree.get(child) - 1);
+                if (inDegree.get(child) == 0) {
+                    sources.add(child);
+                }
+            }
+        }
+
+        return sortedOrder.size() == numCourses;
     }
 }
