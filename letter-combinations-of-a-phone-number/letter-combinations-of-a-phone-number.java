7350b4d0818579553e7d233c6855229c8883c87c
Daniel Leskosky <dleskosky@gmail.com>
2021-04-28 23:20:58 -0400

    Time: 0 ms (100.00%), Space: 37.5 MB (83.86%) - LeetHub

---
+++ letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -0,0 +1,31 @@
+// n = digits.length()
+// Time: O((4^n) * n)
+// Space: O(n)
+
+class Solution {
+    final String[] keys = new String[]{"", "", "abc", "def", "ghi", "jkl", "mno",
+                                       "pqrs", "tuv", "wxyz"};
+    
+    public List<String> letterCombinations(String digits) {
+        List<String> result = new ArrayList<>();
+        if (digits.length() == 0) return result;
+        
+        backtrack(digits, 0, new StringBuilder(), result);
+        return result;
+    }
+    
+    private void backtrack(String digits, int index, StringBuilder combo, List<String> result) {
+        if (index == digits.length()) {
+            result.add(new String(combo));
+            return;
+        }
+        
+        String currKey = keys[digits.charAt(index) - '0'];
+        
+        for (int i = 0; i < currKey.length(); i++) {
+            combo.append(currKey.charAt(i));
+            backtrack(digits, index + 1, new StringBuilder(combo), result);
+            combo.deleteCharAt(combo.length() - 1);
+        }
+    }
+}
