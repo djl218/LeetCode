2d4fb5ccca1de6d40c330e941a145817e1a2af57
Daniel Leskosky <dleskosky@gmail.com>
2021-05-05 21:18:31 -0400

    Time: 0 ms (100.00%), Space: 37.2 MB (84.58%) - LeetHub

---
+++ text-justification/text-justification.java
@@ -0,0 +1,53 @@
+// n = words.length
+// Time: O(n)
+// Space: O(1) - ignore space for output List
+class Solution {
+    public List<String> fullJustify(String[] words, int maxWidth) {
+        List<String> result = new ArrayList<>();
+        int idx = 0;
+        while (idx < words.length) {
+            int count = words[idx].length();
+            int last = idx + 1;
+            
+            while (last < words.length) {
+                if (words[last].length() + count + 1 > maxWidth)
+                    break;
+                
+                count += 1 + words[last].length();
+                last++;
+            }
+            
+            StringBuilder sb = new StringBuilder();
+            sb.append(words[idx]);
+            int diff = last - idx - 1;
+            
+            if (last == words.length || diff == 0) {
+                for (int i = idx + 1; i < last; i++) {
+                    sb.append(" ");
+                    sb.append(words[i]);
+                }
+                for (int i = sb.length(); i < maxWidth; i++) {
+                    sb.append(" ");
+                }
+            } else {
+                int spaces = (maxWidth - count) / diff;
+                int r = (maxWidth - count) % diff;
+                for (int i = idx + 1; i < last; i++) {
+                    for (int s = spaces; s > 0; s--) {
+                        sb.append(" ");
+                    }
+                    if (r > 0) {
+                        sb.append(" ");
+                        r--;
+                    }
+                    sb.append(" ");
+                    sb.append(words[i]);
+                }
+            }
+            result.add(sb.toString());
+            idx = last;
+        }
+        
+        return result;
+    }
+}
