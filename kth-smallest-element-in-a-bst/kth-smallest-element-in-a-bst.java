bbaa79da13835d36e62b654ced147337a79dc381
Daniel Leskosky <dleskosky@gmail.com>
2021-05-11 13:41:37 -0400

    Time: 1 ms (30.34%), Space: 42.4 MB (5.38%) - LeetHub

--- kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
+++ kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
@@ -13,6 +13,11 @@
  *     }
  * }
  */
+/*
+// n = number of nodes
+// Time: O(n)
+// Space: O(n)
+// Every node needs to be iterated and then stored in inorder list
 class Solution {
     public int kthSmallest(TreeNode root, int k) {
         List<Integer> list = new ArrayList<>();
@@ -25,5 +30,26 @@ class Solution {
         inOrder(node.left, list);
         list.add(node.val);
         inOrder(node.right, list);
+    }
+}
+*/
+// Solution inspired by super smart official LeetCode solution
+// n = number of nodes
+// h = height of tree = log(n) for average balanced tree
+// Time: O(h + k) but could be O(n + k) for unbalanced tree
+// Space: O(h) but could be O(n) for unbalanced tree
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        ArrayDeque<TreeNode> stack = new ArrayDeque<>();
+        
+        while (true) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (--k == 0) return root.val;
+            root = root.right;
+        }
     }
 }
