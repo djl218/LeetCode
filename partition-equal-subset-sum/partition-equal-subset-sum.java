30bbbd457a476e9fc9c03cabbd1686acd46df70c
Daniel Leskosky <dleskosky@gmail.com>
2021-05-13 11:25:17 -0400

    Update partition-equal-subset-sum.java

--- partition-equal-subset-sum/partition-equal-subset-sum.java
+++ partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -5,8 +5,7 @@
 class Solution {
     public boolean canPartition(int[] nums) {
         int sum = 0;
-        for (int num : nums) sum += num;
-        
+        for (int num : nums) sum += num;      
         if (sum % 2 != 0) return false;
         
         // the target sum is the total
