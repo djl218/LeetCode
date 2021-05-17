f125a16136952bdaab9aef23be0cb4578f977df2
Daniel Leskosky <dleskosky@gmail.com>
2021-05-17 09:20:15 -0400

    Time: 1 ms (29.18%), Space: 35.8 MB (51.92%) - LeetHub

--- maximum-swap/maximum-swap.java
+++ maximum-swap/maximum-swap.java
@@ -1,6 +1,14 @@
-// 0 <= num <= 10^8, stNum.length <= 9
-// Time: O(9) = O(1)
-// Space: O(9) = O(1)
+/* 
+    0 <= num <= 10^8, stNum.length <= 9
+    Because stNum.length is at most 9 digits, the time and space
+    can be considered constant. However if it was possile for the input
+    to be much longer, then this algorithm would not be ideal, as it would
+    be O(n^2) time with n being stNum.length.
+    
+    Many of the top solutions on the discussion board had "O(n^2)" time as
+    well, so I think that it is acceptable for this use case because it is 
+    guaranteed that there won't be more than 9 digits in the input.
+*/
 class Solution {
     public int maximumSwap(int num) {
         int result = num;
