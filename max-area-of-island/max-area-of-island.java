d2b0f406575bb7a03f2766fd39526cd7d504f00e
Daniel Leskosky <dleskosky@gmail.com>
2021-05-29 16:16:31 -0400

    Time: 2 ms (99.50%), Space: 39.4 MB (72.21%) - LeetHub

--- max-area-of-island/max-area-of-island.java
+++ max-area-of-island/max-area-of-island.java
@@ -1,42 +1,38 @@
-// Time: O(rows * columns)
-// Space: O(rows * columns)
+// m = grid.length, n = grid[0].length
+// Time: O(m * n)
+// Space: O(m * n)
 class Solution {
-    private static final int[] dirX = {0, 1, 0, -1};
-    private static final int[] dirY = {1, 0, -1, 0};
-        
-    private boolean isValid(int nr, int nc, int[][] grid) {
-        return nr >= 0 && nc >= 0 && nr < grid.length && nc < grid[0].length && grid[nr][nc] == 1;
-    }
-        
-    private int bfs (int i, int j, int[][] grid) {
-        Queue<int[]> q = new ArrayDeque<>();
-        q.offer(new int[] {i, j});
-        int count = 0;
-        while (!q.isEmpty()) {
-            int[] current = q.poll();
-            count++;
-            for (int k = 0; k < dirX.length; k++) {
-                int nr = current[0] + dirX[k];
-                int nc = current[1] + dirY[k];
-                if (isValid(nr, nc, grid)) {
-                    grid[nr][nc] = 0;
-                    q.offer(new int[]{nr, nc});
-                }
-            }
-        }
-        return count;
-    }
-        
+    int area, max = 0;
+    int m, n;
+    
     public int maxAreaOfIsland(int[][] grid) {
-        int max = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
+        m = grid.length;
+        n = grid[0].length;
+        boolean[][] visited = new boolean[m][n];
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 1) {
-                    grid[i][j] = 0;
-                    max = Math.max(max, bfs(i, j, grid));
+                    area = 0;
+                    dfs(grid, visited, i, j);
+                    max = Math.max(max, area);
                 }
             }
         }
+        
         return max;
+    }
+    
+    private void dfs(int[][] grid, boolean[][] visited, int i, int j) {
+        if (i < 0 || i >= m || j < 0 || j >= n || visited[i][j] || grid[i][j] != 1)
+            return;
+        
+        area++;
+        visited[i][j] = true;
+        
+        dfs(grid, visited, i + 1, j);
+        dfs(grid, visited, i, j + 1);
+        dfs(grid, visited, i - 1, j);
+        dfs(grid, visited, i, j - 1);
     }
 }
