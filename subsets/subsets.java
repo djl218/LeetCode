363a5eb1b3d1e84dd25e24c9ad041d37ab77970a
Daniel Leskosky <dleskosky@gmail.com>
2021-05-29 15:38:47 -0400

    Update subsets.java

--- subsets/subsets.java
+++ subsets/subsets.java
@@ -1,3 +1,26 @@
+// Cascading
+// n = nums.length
+// Time: O(n * 2^n)
+// Space: O(n * 2^n)
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        result.add(new ArrayList<>());
+        
+        for (int num : nums) {
+            int size = result.size();
+            for (int i = 0; i < size; i++) {
+                List<Integer> newList = new ArrayList<>(result.get(i));
+                newList.add(num);
+                result.add(newList);
+            }
+        }
+        
+        return result;
+    }
+}
+
+// Backtracking
 // n = nums.length
 // Time: O(n * 2^n)
 // Space: O(n)
