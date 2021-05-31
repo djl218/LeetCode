821d2d6ef36d31fc0942f96ea6892e88f7010033
Daniel Leskosky <dleskosky@gmail.com>
2021-05-31 15:36:35 -0400

    Time: 13 ms (22.93%), Space: 40.8 MB (18.68%) - LeetHub

---
+++ basic-calculator/basic-calculator.java
@@ -0,0 +1,44 @@
+// n = s.length()
+// Time: O(n)
+// Space: O(1)
+class Solution {
+    int i = 0;
+    
+    public int calculate(String s) {
+        int n = s.length();
+        boolean isNeg = false;
+        int sum = 0;
+        while (i < n && s.charAt(i) != ')') {
+            char c = s.charAt(i);
+            if (Character.isWhitespace(c)) {
+                i++;
+            } else if (c == '-') {
+                isNeg = true;
+                i++;
+            } else if (c == '+') {
+                isNeg = false;
+                i++;
+            } else if (Character.isDigit(c)) {
+                int currNum = 0;
+                while (i < n && Character.isDigit(s.charAt(i))) {
+                    currNum = currNum * 10 + s.charAt(i) - '0';
+                    i++;
+                }
+                if (isNeg) {
+                    sum -= currNum;
+                } else {
+                    sum += currNum;
+                }
+            } else if (c == '(') {
+                i++;
+                if (isNeg) {
+                    sum -= calculate(s);
+                } else {
+                    sum += calculate(s);
+                }
+                i++;
+            }
+        }
+        return sum;
+    }
+}
