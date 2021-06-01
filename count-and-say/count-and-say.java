05549db87aa38fc41352ca617ac6b3ed6dafa470
Daniel Leskosky <dleskosky@gmail.com>
2021-05-31 22:37:10 -0400

    Time: 4 ms (58.68%), Space: 36.6 MB (65.96%) - LeetHub

---
+++ count-and-say/count-and-say.java
@@ -0,0 +1,28 @@
+//  m = curr.length();
+// Time: O(n * m)
+// Space: O(m)
+class Solution {
+    public String countAndSay(int n) {
+        Map<Character, Integer> map = new HashMap<>();
+        String curr = "1";
+        while (n-- > 1) {
+            StringBuilder sb = new StringBuilder();
+            char c = curr.charAt(0);
+            int count = 1;
+            for (int i = 1; i < curr.length(); i++) {
+                if (curr.charAt(i) == c) {
+                    count++;
+                } else {
+                    sb.append(count);
+                    sb.append(c);
+                    c = curr.charAt(i);
+                    count = 1;
+                }
+            }
+            sb.append(count);
+            sb.append(c);
+            curr = sb.toString();
+        }
+        return curr;
+    }
+}
