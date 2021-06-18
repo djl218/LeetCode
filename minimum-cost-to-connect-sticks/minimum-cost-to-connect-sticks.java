39628705fb2674d07430f409592b9ddb1fb716b9
Daniel Leskosky <dleskosky@gmail.com>
2021-06-18 19:10:36 -0400

    Time: 59 ms (56.27%), Space: 40 MB (42.84%) - LeetHub

---
+++ minimum-cost-to-connect-sticks/minimum-cost-to-connect-sticks.java
@@ -0,0 +1,22 @@
+// n = sticks.length
+// Time: O(n log n)
+// Space: O(n)
+class Solution {
+    public int connectSticks(int[] sticks) {
+        int totalCost = 0;
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        
+        for (int stick : sticks) {
+            minHeap.offer(stick);
+        }
+        
+        while (minHeap.size() > 1) {
+            int smallest = minHeap.poll();
+            int nextSmallest = minHeap.poll();
+            int cost = smallest + nextSmallest;
+            totalCost += cost;
+            minHeap.offer(cost);
+        }
+        return totalCost;
+    }
+}
