96bef22a20ee8aa99ea905d1622a77a0d8681740
Daniel Leskosky <dleskosky@gmail.com>
2021-07-11 11:38:39 -0400

    Update paint-house.java

--- paint-house/paint-house.java
+++ paint-house/paint-house.java
@@ -1,6 +1,7 @@
+// Top-Down DP with Memoization
 // n = costs.length
-// Time: O(3n) = O(n)
-// Space: O(1)
+// Time: O(n)
+// Space: O(n)
 class Solution {
     private int[][] costs;
     private int[][] memo;
@@ -27,5 +28,23 @@ class Solution {
         }
         memo[house][color] = totalCost;
         return memo[house][color];
+    }
+}
+
+
+// Bottom-Up DP with Tabulation
+// n = costs.length
+// Time: O(n)
+// Space: O(1)
+class Solution {
+    public int minCost(int[][] costs) {
+        int n = costs.length;
+        for (int i = 1; i < n; i++) {
+            costs[i][0] += Math.min(costs[i - 1][1], costs[i - 1][2]);
+            costs[i][1] += Math.min(costs[i - 1][0], costs[i - 1][2]);
+            costs[i][2] += Math.min(costs[i - 1][0], costs[i - 1][1]);
+        }
+        
+        return Math.min(Math.min(costs[n - 1][0], costs[n - 1][1]), costs[n - 1][2]);
     }
 }
