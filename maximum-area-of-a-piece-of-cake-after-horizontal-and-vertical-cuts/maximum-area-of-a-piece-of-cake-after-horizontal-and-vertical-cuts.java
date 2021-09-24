0d636ad3d604b08b027df74fa629a4c83e708f12
Daniel Leskosky <dleskosky@gmail.com>
2021-09-24 08:28:30 -0400

    Time: 68 ms (5.25%), Space: 57.1 MB (32.95%) - LeetHub

--- maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.java
+++ maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.java
@@ -1,25 +1,41 @@
 // n = horizontalCuts.length, m = verticalCuts.length
-// Time: O(nlogn + mlogm)
-// Space: O(1)
+// Time: O((n log n) + (m log m))
+// Space: O(n + m)
 class Solution {
     public int maxArea(int h, int w, int[] horizontalCuts, int[] verticalCuts) {
-        int lenH = horizontalCuts.length;
-        int lenV = verticalCuts.length;
+        Set<Integer> setH = new HashSet<>();
+        Set<Integer> setV = new HashSet<>();
         
-        Arrays.sort(horizontalCuts);
-        Arrays.sort(verticalCuts);
-        
-        long maxH = Math.max(horizontalCuts[0], h - horizontalCuts[lenH - 1]);
-        for (int i = 1; i < lenH; i++) {
-            maxH = Math.max(maxH, horizontalCuts[i] - horizontalCuts[i - 1]);
+        for (int hCut : horizontalCuts) {
+            setH.add(hCut);
+        }
+        for (int vCut : verticalCuts) {
+            setV.add(vCut);
         }
         
-        long maxV = Math.max(verticalCuts[0], w - verticalCuts[lenV - 1]);
-        for (int i = 1; i < lenV; i++) {
-            maxV = Math.max(maxV, verticalCuts[i] - verticalCuts[i - 1]);
+        List<Integer> listH = new ArrayList<>(setH);
+        List<Integer> listV = new ArrayList<>(setV);
+        
+        listH.add(0, 0);
+        listV.add(0, 0);
+        listH.add(h);
+        listV.add(w);
+        
+        Collections.sort(listH);
+        Collections.sort(listV);
+        
+        long maxDiffH = 0;
+        for (int i = 1; i < listH.size(); i++) {
+            int diff = listH.get(i) - listH.get(i - 1);
+            maxDiffH = Math.max(maxDiffH, diff);
         }
         
-        long mod = 1_000_000_007;
-        return (int) ((maxH * maxV) % mod); 
+        long maxDiffV = 0;
+        for (int i = 1; i < listV.size(); i++) {
+            int diff = listV.get(i) - listV.get(i - 1);
+            maxDiffV = Math.max(maxDiffV, diff);
+        }
+        
+        return (int) ((maxDiffH * maxDiffV) % (1000000007));
     }
 }
