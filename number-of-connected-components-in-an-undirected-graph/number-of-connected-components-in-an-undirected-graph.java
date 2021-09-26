0fdfadf8c4d9d0ff733b807550689295eb2c38eb
Daniel Leskosky <dleskosky@gmail.com>
2021-09-26 10:54:18 -0400

    Time: 25 ms (6.56%), Space: 40.1 MB (46.32%) - LeetHub

--- number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.java
+++ number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.java
@@ -1,50 +1,34 @@
+// E = number of edges, V = number of vertices
+// Time: O(E + V)
+// Space: O(E + V)
 class Solution {
+    Set<Integer> visited;
     public int countComponents(int n, int[][] edges) {
-        UnionFind uf = new UnionFind(n);
-        for (int[] edge : edges)
-            uf.union(edge[0], edge[1]);
-        
-        return uf.getCount();
-    }
-}
-
-class UnionFind {
-    private int[] parent;
-    private int[] rank;
-    private int count;
-    
-    public UnionFind(int n) {
-        parent = new int[n];
-        rank = new int[n];
-        count = n;
-        for (int i = 0; i < n; i++)
-            parent[i] = i;
-    }
-    
-    private int find(int p) {
-        while (p != parent[p]) {
-            parent[p] = parent[parent[p]];
-            p = parent[p];
+        Map<Integer, List<Integer>> adj = new HashMap<>();
+        int count = 0;
+        for (int[] edge : edges) {
+            adj.putIfAbsent(edge[0], new ArrayList<>());
+            adj.get(edge[0]).add(edge[1]);
+            adj.putIfAbsent(edge[1], new ArrayList<>());
+            adj.get(edge[1]).add(edge[0]);
         }
-        return p;
-    }
-    
-    public void union(int p, int q) {
-        int rootP = find(p);
-        int rootQ = find(q);
-        if (rootP == rootQ) return;
-        if (rank[rootP] < rank[rootQ]) {
-            parent[rootP] = rootQ;
-        } else {
-            parent[rootQ] = rootP;
-            if (rank[rootP] == rank[rootQ]) {
-                rank[rootQ]++;
+        visited = new HashSet<>();
+        for (int node : adj.keySet()) {
+            System.out.println(node);
+            if (!visited.contains(node)) {
+                count++;
+                dfs(adj, node);
             }
         }
-        count--;
+        return n > adj.size() ? count + (n - adj.size()) : count;
     }
     
-    public int getCount() {
-        return count;
+    private void dfs(Map<Integer, List<Integer>> adj, int node) {
+        visited.add(node);
+        for (int nei : adj.get(node)) {
+            if (!visited.contains(nei)) {
+                dfs(adj, nei);
+            }
+        }
     }
 }
