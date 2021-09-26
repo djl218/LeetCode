77431ffab3f591beb46355d67ab35a7a492fccc4
Daniel Leskosky <dleskosky@gmail.com>
2021-09-26 17:34:12 -0400

    Time: 5 ms (85.55%), Space: 42.3 MB (86.69%) - LeetHub

--- shortest-path-to-get-food/shortest-path-to-get-food.java
+++ shortest-path-to-get-food/shortest-path-to-get-food.java
@@ -2,53 +2,60 @@
 // Time: O(m * n)
 // Space: O(m * n)
 class Solution {
-    int[][] directions = new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
     int m, n;
+    boolean[][] visited;
+    int[][] dirs = new int[][]{{0,1},{1,0},{0,-1},{-1,0}};
+    char[][] grid;
     
     public int getFood(char[][] grid) {
+        this.grid = grid;
         m = grid.length;
         n = grid[0].length;
+        visited = new boolean[m][n];
         
-        for (int i = 0; i < m; i++) {
+        int startX = 0, startY = 0;
+        boolean found = false;
+        for (int i = 0; i < m && !found; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == '*') {
-                    return bfs(grid, new boolean[m][n], i, j);
+                    startX = i;
+                    startY = j;
+                    found = true;
+                    break;
                 }
             }
+        }
+        
+        Queue<int[]> q = new LinkedList<>();
+        visited[startX][startY] = true;
+        q.offer(new int[]{startX, startY});
+        
+        int shortest = 0;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                int[] curr = q.poll();
+                int x = curr[0];
+                int y = curr[1];
+                if (grid[x][y] == '#') {
+                    return shortest;
+                }
+                for (int[] dir : dirs) {
+                    int nextX = x + dir[0];
+                    int nextY = y + dir[1];
+                    if (inBounds(nextX, nextY)) {
+                        q.offer(new int[]{nextX, nextY});
+                        visited[nextX][nextY] = true;
+                    }
+                }
+            }
+            shortest++;
         }
         
         return -1;
     }
     
-    private int bfs(char[][] grid, boolean[][] visited, int row, int col) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{row, col});
-        
-        int steps = 0;
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                int[] curr = queue.poll();
-                if (grid[curr[0]][curr[1]] == '#') {
-                    return steps;
-                }
-                for (int j = 0; j < 4; j++) {
-                    int nextX = curr[0] + directions[j][0];
-                    int nextY = curr[1] + directions[j][1];
-                    if (nextX < 0 || nextX >= m || nextY < 0 || nextY >= n
-                        || visited[nextX][nextY] || grid[nextX][nextY] == 'X')
-                    {
-                        continue;
-                    }
-                    if (grid[nextX][nextY] == 'O' || grid[nextX][nextY] == '#') {
-                        visited[nextX][nextY] = true;
-                        queue.offer(new int[]{nextX, nextY});
-                    }
-                }
-            }
-            steps++;
-        }
-        
-        return -1;
+    private boolean inBounds(int x, int y) {
+        return x >= 0 && x < m && y >= 0 && y < n && !visited[x][y] && grid[x][y] != 'X';
     }
 }
