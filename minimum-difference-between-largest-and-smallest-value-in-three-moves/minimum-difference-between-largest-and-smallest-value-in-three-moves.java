c58114d544cc692f6824fb2b832f9da43089de67
Daniel Leskosky <dleskosky@gmail.com>
2021-10-04 07:32:21 -0400

    Time: 74 ms (5.27%), Space: 69.2 MB (5.02%) - LeetHub

--- minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.java
+++ minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.java
@@ -1,34 +1,44 @@
-// n = nums.length
-// Time: O(n)
+// N = nums.length
+// Time: O(N * lg4 * 2) == O(N), if N > 8;
+//       O(NlogN) if N <= 8;
 // Space: O(1)
 class Solution {
     public int minDifference(int[] nums) {
-        if (nums.length <= 4) return 0;
-        
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>((a, b) -> b - a);
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>();
-        
+        if (nums.length <= 4) {
+            return 0;
+        }
+        if (nums.length <= 8) {
+            return getDiff(nums, true);
+        }
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
         for (int num : nums) {
             minHeap.offer(num);
             maxHeap.offer(num);
             if (minHeap.size() > 4) {
                 minHeap.poll();
+            }
+            if (maxHeap.size() > 4) {
                 maxHeap.poll();
             }
         }
-        
-        int[] mins = new int[4];
-        int[] maxes = new int[4];
-        for (int i = 0, j = 3; i < 4; i++, j--) {
-            mins[i] = minHeap.poll();
-            maxes[j] = maxHeap.poll();
+        int[] arr = new int[8];
+        for (int i = 3, j = 4; i >= 0 && j < 8; i--, j++) {
+            arr[i] = maxHeap.poll();
+            arr[j] = minHeap.poll();
         }
-        
-        int minDiff = Integer.MAX_VALUE;
+        return getDiff(arr, false);
+    }
+    
+    private int getDiff(int[] arr, boolean needSort) {
+        if (needSort) {
+            Arrays.sort(arr);
+        }
+        int n = arr.length;
+        int min = Integer.MAX_VALUE;
         for (int i = 0; i < 4; i++) {
-            minDiff = Math.min(minDiff, Math.abs(maxes[i] - mins[i]));
+            min = Math.min(min, arr[n - 4 + i] - arr[i]);
         }
-        
-        return minDiff;
+        return min;
     }
 }
