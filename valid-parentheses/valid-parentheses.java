88105cee1a696d425f14c9c798027bb2ef0200b7
Daniel Leskosky <dleskosky@gmail.com>
2021-10-08 08:59:11 -0400

    Time: 1 ms (99.01%), Space: 37.3 MB (60.30%) - LeetHub

--- valid-parentheses/valid-parentheses.java
+++ valid-parentheses/valid-parentheses.java
@@ -1,26 +1,24 @@
-// n = s.length()
+// n = s.length
 // Time: O(n)
 // Space: O(n)
 class Solution {
     public boolean isValid(String s) {
+        int n = s.length();
         Stack<Character> stack = new Stack<>();
-        
-        for (char c : s.toCharArray()) {
-            if (!stack.isEmpty()) {
-                if (c == ')' && stack.peek() == '(') {
-                    stack.pop();
-                } else if (c == ']' && stack.peek() == '[') {
-                    stack.pop();
-                } else if (c == '}' && stack.peek() == '{') {
-                    stack.pop();
-                } else {
-                    stack.push(c);
-                }
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            if (stack.isEmpty()) {
+                stack.push(c);
+            } else if (c == ')' && stack.peek() == '(') {
+                stack.pop();
+            } else if (c == ']' && stack.peek() == '[') {
+                stack.pop();
+            } else if (c == '}' && stack.peek() == '{') {
+                stack.pop();
             } else {
                 stack.push(c);
             }
         }
-        
         return stack.isEmpty();
     }
 }
