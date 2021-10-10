5d395f78cfdcb77bf33515172faf7b4daebbd515
Daniel Leskosky <dleskosky@gmail.com>
2021-10-10 19:09:07 -0400

    Update compare-version-numbers.java

--- compare-version-numbers/compare-version-numbers.java
+++ compare-version-numbers/compare-version-numbers.java
@@ -1,34 +1,29 @@
-// v1 = version.length(), v2 = version2.length()
-// max = Max(version1.length(), version2.length())
-// Time: O(v1 + v2 + max)
-// Space: O(v1 + v2 + max)
+// n = version1.length, m = version2.length
+// Time: O(m + n)
+// Space: O(m + n)
 class Solution {
     public int compareVersion(String version1, String version2) {
-        String[] v1Arr = version1.split("\\.");
-        String[] v2Arr = version2.split("\\.");
-        
-        int longer = Math.max(v1Arr.length, v2Arr.length);
-        
-        List<Integer> v1List = new ArrayList<>();
-        List<Integer> v2List = new ArrayList<>();
-        
-        int i1 = 0;
-        for (; i1 < v1Arr.length; i1++) {
-            v1List.add(Integer.valueOf(v1Arr[i1]));
+        String[] v1arrStr = version1.split("\\.");
+        String[] v2arrStr = version2.split("\\.");
+        int n = Math.max(v1arrStr.length, v2arrStr.length);
+        int[] v1arrInt = new int[n];
+        int[] v2arrInt = new int[n];
+        for (int i = 0; i < n; i++) {
+            if (i < v1arrStr.length) {
+                v1arrInt[i] = Integer.valueOf(v1arrStr[i]);
+            }
+            if (i < v2arrStr.length) {
+                v2arrInt[i] = Integer.valueOf(v2arrStr[i]);
+            }
         }
-        while (i1++ < longer) v1List.add(0);
-        
-        int i2 = 0;
-        for (; i2 < v2Arr.length; i2++) {
-            v2List.add(Integer.valueOf(v2Arr[i2]));
+        for (int i = 0; i < n; i++) {
+            if (v1arrInt[i] < v2arrInt[i]) {
+                return -1;
+            }
+            if (v1arrInt[i] > v2arrInt[i]) {
+                return 1;
+            }
         }
-        while (i2++ < longer) v2List.add(0);
-        
-        for (int i12 = 0; i12 < v1List.size(); i12++) {
-            if (v1List.get(i12) < v2List.get(i12)) return -1;
-            if (v1List.get(i12) > v2List.get(i12)) return 1;
-        }
-        
         return 0;
     }
 }
