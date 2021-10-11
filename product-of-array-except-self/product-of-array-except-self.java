5709d278061700cb7eb4695368d515ec3967c2bf
Daniel Leskosky <dleskosky@gmail.com>
2021-10-11 19:40:52 -0400

    Update product-of-array-except-self.java

--- product-of-array-except-self/product-of-array-except-self.java
+++ product-of-array-except-self/product-of-array-except-self.java
@@ -5,20 +5,15 @@ class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
         int[] result = new int[n];
-        result[0] = 0;
+        result[0] = 1;
         for (int i = 1; i < n; i++) {
-            if (i == 1) {
-                result[i] = nums[i - 1];
-            } else {
-                result[i] = nums[i - 1] * result[i - 1];
-            }
+            result[i] = nums[i - 1] * result[i - 1];
         }
-        int prev = nums[n - 1];
-        for (int i = n - 2; i >= 1; i--) {
+        int prev = 1;
+        for (int i = n - 1; i >= 0; i--) {
             result[i] = prev * result[i];
             prev *= nums[i];
         }
-        result[0] = prev;
         return result;
     }
 }
