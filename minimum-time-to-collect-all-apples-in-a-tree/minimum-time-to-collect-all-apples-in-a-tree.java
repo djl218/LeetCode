f6aef3cb1f5c794e02bc78693224b8bec65624bd
Daniel Leskosky <dleskosky@gmail.com>
2021-10-12 07:23:19 -0400

    Time: 94 ms (27.65%), Space: 116.1 MB (13.97%) - LeetHub

---
+++ minimum-time-to-collect-all-apples-in-a-tree/minimum-time-to-collect-all-apples-in-a-tree.java
@@ -0,0 +1,32 @@
+// n = number of nodes
+// Time: O(n)
+// Space: O(n)
+class Solution {    
+    Set<Integer> visited = new HashSet<>();
+    
+    public int minTime(int n, int[][] edges, List<Boolean> hasApple) {
+        Map<Integer, List<Integer>> adj = new HashMap<>();
+        for (int[] edge : edges) {
+            adj.putIfAbsent(edge[0], new ArrayList<>());
+            adj.putIfAbsent(edge[1], new ArrayList<>());
+            adj.get(edge[0]).add(edge[1]);
+            adj.get(edge[1]).add(edge[0]);
+        }
+        return dfs(0, adj, hasApple, 0);
+    }
+    
+    private int dfs(int parent, Map<Integer, List<Integer>> adj, List<Boolean> hasApple, int oneEdgeTime) {
+        if (visited.contains(parent)) {
+            return 0;
+        }
+        visited.add(parent);
+        int allEdgeTime = 0;
+        for (int child : adj.get(parent)) {
+            allEdgeTime += dfs(child, adj, hasApple, 2);
+        }
+        if (allEdgeTime == 0 && !hasApple.get(parent)) {
+            return 0;
+        }
+        return allEdgeTime + oneEdgeTime;
+    }
+}
