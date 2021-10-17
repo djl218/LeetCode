14d5dd0dc03db2ab4ed75e3d4f9b857d30b245d2
Daniel Leskosky <dleskosky@gmail.com>
2021-10-17 13:33:58 -0400

    Time: 36 ms (21.62%), Space: 40.1 MB (80.08%) - LeetHub

--- task-scheduler/task-scheduler.java
+++ task-scheduler/task-scheduler.java
@@ -1,23 +1,36 @@
-// The O(x log x) time for sort can be neglected because the sort
-// is done on counts, which is constant length
-
-// n = tasks.length
-// Time: O(n)
-// Space: (1)
+// Time : O(n)
+// Space: O(1)
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-        int[] counts = new int[26];
-        for (char c : tasks) counts[c - 'A']++;
-        Arrays.sort(counts);
+        if (n == 0) return tasks.length;
         
-        int maxCount = counts[25];
-        int idleTime = (maxCount - 1) * n;
-        
-        for (int i = counts.length - 2; i >= 0 && idleTime > 0; i--) {
-            idleTime -= Math.min(maxCount - 1, counts[i]);
+        int[] taskTracker = new int[26];
+        for (char task : tasks) {
+            taskTracker[task - 'A']++;
         }
-        idleTime = Math.max(0, idleTime);
         
-        return idleTime + tasks.length;
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+        for (int count : taskTracker) {
+            if (count != 0) {
+                pq.offer(count);
+            }
+        }
+        
+        Map<Integer, Integer> coolDown = new HashMap<>();
+        int time = 0;
+        while (!pq.isEmpty() || !coolDown.isEmpty()) {
+            if (coolDown.containsKey(time - n - 1)) {
+                pq.offer(coolDown.get(time - n - 1));
+                coolDown.remove(time - n - 1);
+            }
+            if (!pq.isEmpty()) {
+                int polled = pq.poll() - 1;
+                if (polled != 0) {
+                    coolDown.put(time, polled);
+                }
+            }
+            time++;
+        }
+        return time;
     }
 }
